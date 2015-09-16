febd33b2102c43c15f4b2802d60fba016052472a
Pedro <pncordeiro@gmail.com>
2015-09-16 12:37:20 +0100

    Collision detected. Explosions not working properly

---
+++ Scripts/DestroyByContact.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+	public GameObject explosion;
+
+	void OnTriggerEnter(Collider other)
+	{
+		Instantiate (explosion, transform.position, transform.rotation);
+
+		Destroy(other.gameObject);
+		Destroy(gameObject);
+	}
+
+}
