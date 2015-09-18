342fa076d1e573792cb2ed0f4af2664bc6da60d2
Pedro <pncordeiro@gmail.com>
2015-09-18 11:47:55 +0100

    Laser reflection fixed

--- Scripts/LaserTrajectory.cs
+++ Scripts/LaserTrajectory.cs
@@ -4,22 +4,35 @@ using System.Collections;
 public class LaserTrajectory : MonoBehaviour {
 
 	private Rigidbody rb;
+	private Vector3 newDir;	// New direction
+
 	public float speed;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		rb = GetComponent<Rigidbody>();	
 		rb.velocity = transform.forward * speed;
+
+		newDir = transform.forward; // Gets the laser direction
 	}
 
-	void OnTriggerEnter(Collider other){
-
-		Debug.Log("Old rotation: " + transform.rotation.eulerAngles);
-
+	void OnTriggerEnter(Collider other)
+	{
 		Vector3 oldRotation = transform.rotation.eulerAngles;
 		transform.Rotate (0, -(2*oldRotation.y), 0);	// Laser collision on side walls
-		rb.velocity = transform.forward * speed;
-		
-		Debug.Log("New rotation: " + transform.rotation.eulerAngles);
+
+		if (other.tag == "Walls1") // Side walls
+		{
+			newDir.x = -newDir.x;
+		}		
+		else
+		{
+			if(other.tag == "Walls2") // Up/bottom walls
+			{
+				newDir.z = -newDir.z;
+			}
+		}
+		rb.velocity = newDir * speed;
 	}
 }
