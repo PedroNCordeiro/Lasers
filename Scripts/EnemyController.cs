8dc7103c30484f4dcd77935edab22f0fc1db129d
Pedro <pncordeiro@gmail.com>
2015-09-18 16:03:51 +0100

    Enemy dodging improved

--- Scripts/EnemyController.cs
+++ Scripts/EnemyController.cs
@@ -13,6 +13,7 @@ public class EnemyController : MonoBehaviour {
 	private Rigidbody rb;
 	private GameObject nearestLaser;
 	private float nextFire;
+	private float nearestLaserDist; // Distance to the nearest laser
 
 	public Transform bestSpot; // Default position of the ship, if there are no lasers nearby
 	public float speed;
@@ -22,26 +23,42 @@ public class EnemyController : MonoBehaviour {
 	public float fireRate;
 	public GameObject laser;
 	public Transform laserSpawn;
-	
+
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody>();
+		nearestLaserDist = Mathf.Infinity;
 	}
 
 	void Update ()
 	{
-		Vector3 targetDir = humanPlayer.position - transform.position;
 		float step = speed * Time.deltaTime;
-		Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
-		transform.rotation = Quaternion.LookRotation(newDir);
 
-		nearestLaser = GetClosestLaser();
-
-		if (nearestLaser == null || Vector3.Distance(transform.position, nearestLaser.transform.position) > safetyDistance) 
-			transform.position = Vector3.MoveTowards(transform.position, bestSpot.position, step); // Move to best spot
-		else
+		// Rotate to the Human Player Ship
+		if (humanPlayer != null)
 		{
-			transform.position = Vector3.MoveTowards(transform.position, nearestLaser.transform.position, -step); // Run away from the laser
+			Vector3 targetDir = humanPlayer.position - transform.position;
+			Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
+			transform.rotation = Quaternion.LookRotation(newDir);
+		}
+
+		// Find nearest laser shot
+		nearestLaser = GetClosestLaser();
+		if (nearestLaser != null)
+			nearestLaserDist = Vector3.Distance(transform.position, nearestLaser.transform.position);
+		
+		// Move to best spot if no lasers nearby
+		if (nearestLaser == null || nearestLaserDist > safetyDistance + 1) 
+			transform.position = Vector3.MoveTowards(transform.position, bestSpot.position, step/3); 
+		else
+		// Run away from nearest laser
+		{
+			if (nearestLaserDist < safetyDistance - 1)
+			{
+				Vector3 dodgeLaser = Vector3.MoveTowards(transform.position, nearestLaser.transform.position, -step); // Run away from the laser
+				dodgeLaser += (Vector3.one * Time.deltaTime); // Add some noise (so it doesn't seem so robotic)
+				transform.position = dodgeLaser;
+			}
 		}
 
 		rb.position = new Vector3
@@ -51,12 +68,13 @@ public class EnemyController : MonoBehaviour {
 			Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
 		);
 
-		if (Time.time > nextFire)
-		{
-			nextFire = Time.time + fireRate;
-			// Instantiate shot
-			Instantiate (laser, laserSpawn.position, laserSpawn.rotation);
-		}
+		// // Fire shots whenever possible
+		// if (Time.time > nextFire)
+		// {
+		// 	nextFire = Time.time + fireRate;
+		// 	// Instantiate shot
+		// 	Instantiate (laser, laserSpawn.position, laserSpawn.rotation);
+		// }
 
 	}
 
